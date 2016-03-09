de6baca4bb24dfd562db2aadf66599e0bbd97d34
philiptang42 <philiptang42@gmail.com>
2016-03-08 22:47:41 -0500

    added scope.product

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,9 @@
 app.controller('MainController',
 ['$scope', function($scope) {
   $scope.title = 'My Own Title';
-  $scope.promo = "Austin 3:16";
+  $scope.promo = 'Austin 3:16';
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19
+  }
 }]);
