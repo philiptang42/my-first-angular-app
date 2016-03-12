8024b916ed45b6ea4b7080c423187ef02d68b361
philiptang42 <philiptang42@gmail.com>
2016-03-11 20:50:25 -0500

    used ng-src to produce correcy object image

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,18 +2,18 @@ app.controller('MainController',
 ['$scope', function($scope) {
   $scope.title = 'My Own Title';
   $scope.promo = 'Austin 3:16';
-  $scope.products = [ 
+  $scope.products = [
     {
       name: 'The Book of Trees',
       price: 19,
       pubdate: new Date('2014', '03', '08'),
-      cover: 'img/the-book-of-trees.jpg'
+      cover: 'img/the-book-of-trees.jpeg'
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
-      cover: 'img/program-or-be-programmed.jpg'
+      cover: 'img/program-or-be-programmed.jpeg'
     }
   ]
 }]);
