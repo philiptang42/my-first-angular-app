9cfea2ede18aff25dd389b2b8de2e14fd81923b5
philiptang42 <philiptang42@gmail.com>
2016-03-12 21:01:48 -0500

    adding the like property to each object in scope.products

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,25 +7,29 @@ app.controller('MainController',
       name: 'The Book of Trees',
       price: 19,
       pubdate: new Date('2014', '03', '08'),
-      cover: 'img/the-book-of-trees.jpeg'
+      cover: 'img/the-book-of-trees.jpeg',
+      likes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
-      cover: 'img/program-or-be-programmed.jpeg'
+      cover: 'img/program-or-be-programmed.jpeg',
+      likes: 0
     },
     {
       name: 'Book 3',
       price: 666,
       pubdate: new Date('2000', '06', '06'),
-      cover: ''
+      cover: '',
+      likes: 0
     },
     {
       name: 'Book 4',
       price: 777,
       pubdate: new Date('1999', '12', '12'),
-      cover: ''
+      cover: '',
+      likes: 0
     }
   ]
 }]);
