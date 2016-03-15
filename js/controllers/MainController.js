c9684da22ebb6d13c341773ed205500564903e6c
philiptang42 <philiptang42@gmail.com>
2016-03-14 20:12:20 -0400

    adding dislike property

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,28 +8,32 @@ app.controller('MainController',
       price: 19,
       pubdate: new Date('2014', '03', '08'),
       cover: 'img/the-book-of-trees.jpeg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
       cover: 'img/program-or-be-programmed.jpeg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Book 3',
       price: 666,
       pubdate: new Date('2000', '06', '06'),
       cover: '',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Book 4',
       price: 777,
       pubdate: new Date('1999', '12', '12'),
       cover: '',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }
   ];
     $scope.plusOne = function(index) {
